3a85750309e7f3eb52e00fa51222b9bb44bdbc13
sravancipher <sravanpotnuru24@gmail.com>
2024-05-27 17:29:34 +0530

    updated

---
+++ ScreenSaga_Rest_API/src/test/java/com/rest/screensaga/ScreenSagaRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.screensaga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenSagaRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
