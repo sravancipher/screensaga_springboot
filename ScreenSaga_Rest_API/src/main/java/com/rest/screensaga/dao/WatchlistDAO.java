d136475a2f34d99d776ae73d61b1878103659c7a
sravancipher <sravanpotnuru24@gmail.com>
2024-05-28 09:15:37 +0530

    updated

--- ScreenSaga_Rest_API/src/main/java/com/rest/screensaga/dao/WatchlistDAO.java
+++
@@ -1,9 +0,0 @@
-package com.rest.screensaga.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.rest.screensaga.entities.Watchlist;
-
-public interface WatchlistDAO extends JpaRepository<Watchlist,Integer>{
-
-}
